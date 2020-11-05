3f2975f6a257a1d58147d9eb5bbfa286028d0094
daleinen7 <daleinen@gmail.com>
2020-11-04 20:05:14 -0600

    added logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import {Link} from 'gatsby'
 
+import logo from './logo.png'
+
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  img {
+    width: 400px;
+  }
+  ul {
+    display: flex;
+    flex-direction: row;
+    list-style-type: none;
+    justify-content: space-between;    
+  }
+  
+`;
+
 export default function Header() {
   return(
-    <div>
-      <h1>Logo</h1>
+    <StyledDiv>
+      <h1><Link to='/'><img src={logo} alt="Candy Fluffs Logo"/></Link></h1>
       <p>Sub</p>
-      <ul>
+      <nav>
+        <ul>
           <li>
-              <Link to='/'>Home</Link>
+            <Link to='/'>Home</Link>
           </li>
           <li>
-              <Link to='/shop'>Shop</Link>
+            <Link to='/shop'>Shop</Link>
           </li>
           <li>
-              <Link to='/necahual'>Necahual</Link>
+            <Link to='/necahual'>Necahual</Link>
           </li>
-      </ul>
-    </div>
+        </ul>
+      </nav>
+    </StyledDiv>
   )
 }
