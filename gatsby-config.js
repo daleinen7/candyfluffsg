682c6be9e9259d7148f7b0e043d81e4335ef2de9
daleinen7 <daleinen@gmail.com>
2020-11-07 12:43:19 -0600

    initialize strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`article`, `user`],
+        //If using single types place them in this array.
+        singleTypes: [`home-page`, `contact`],
+        // Possibility to login with a strapi user, when content types are not publically available (optional).
+        loginData: {
+          identifier: "",
+          password: "",
+        },
+      },
+    },
+  ],
 }
